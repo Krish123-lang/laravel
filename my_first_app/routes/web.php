45de748cf0c422408ccd27f43857fa329bf63160
Krishna123-lang <mandalkrishna739@gmail.com>
2023-07-25 16:19:50 +0545

    hwaw

--- my_first_app/routes/web.php
+++ my_first_app/routes/web.php
@@ -6,18 +6,6 @@ use App\Http\Controllers\SingleActionController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\RegistrationController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });

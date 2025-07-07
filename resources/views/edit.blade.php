314bd0989c9f5e023deb0caf8629737d5aba0a15
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-07-07 12:15:20 +0545

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -42,15 +42,14 @@
     <script>
         var input = document.querySelector("#phone");
         var iti = window.intlTelInput(input, {
-            separateDialCode: true, // Shows country code separately
-            initialCountry: "np", // Default country (Nepal)
-            preferredCountries: ["np", "in", "us"], // Preferred countries at the top
+            separateDialCode: true,
+            initialCountry: "np",
+            preferredCountries: ["np", "in", "us"],
             utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
         });
 
-        // When the form is submitted, set the full phone number in a hidden input
         input.form.addEventListener("submit", function() {
-            input.value = iti.getNumber(); // Gets the full number in E.164 format
+            input.value = iti.getNumber(); 
         });
     </script>
 @endpush
